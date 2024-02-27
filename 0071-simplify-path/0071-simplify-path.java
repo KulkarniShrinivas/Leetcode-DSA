6ea40d1fa7e688206f4355b7ee1fffa7594faa6b
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-02-27 21:54:28 +0530

    Time: 4 ms (84.65%), Space: 42.9 MB (94.7%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,18 +1,44 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>(); // create a stack to keep track of directories
-        String[] directories = path.split("/"); // split the path by slash '/'
-        for (String dir : directories) { // iterate over the directories
-            if (dir.equals(".") || dir.isEmpty()) { // ignore the current directory '.' and empty directories
-                continue;
-            } else if (dir.equals("..")) { // go one level up for double period '..'
-                if (!stack.isEmpty()) { // if stack is not empty, pop the top element
-                    stack.pop();
-                }
-            } else { // for any other directory, push it to the stack
-                stack.push(dir);
-            }
-        }
-        return "/" + String.join("/", stack); // join the directories in the stack with slash '/' and add a slash at the beginning
+     Stack<String> s= new Stack<>();
+     StringBuilder res= new StringBuilder();
+     String[] p = path.split("/");
+
+     for(int i=0; i < p.length; i++){
+         if(!s.isEmpty() && p[i].equals("..")) s.pop();
+         else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+            s.push(p[i]);
+     }
+     if(s.isEmpty()) return "/";
+     while(!s.isEmpty()){
+         res.insert(0, s.pop()).insert(0, "/");
+     }
+     return res.toString();
+
+
+
+
     }
 }
+
+//   home--->a->/b->a->home ->c at the end it will go to c
+     //  /a/./b/../../c/ => 
+     // operations pushing and popping 
+     // /.. -> pop(come out of dir)
+     // /name -> push(go inside the dir0)
+
+    // dsa-> stack or doubely ended queue
+
+
+// /a/./b/../.../c/ -> [a][.][b][..][..][c] -> string operation 
+
+//-------
+//c
+//-------     
+
+// to calculate path in doubely ended queue we can just start from the front  
+
+//|      |
+//|      |
+//|  c    |
+//|------|
