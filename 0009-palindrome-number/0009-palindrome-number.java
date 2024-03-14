d6d49ec91ef9f6d5c2882505163ca63cf272f544
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-03-14 21:26:19 +0530

    Time: 7 ms (19.04%), Space: 43.7 MB (77.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-       if(x<0){
-           return false;
-       } 
-       long reversed=0;
-       long temp=x;
-
-       while(temp != 0){
-           int digit = (int) (temp % 10);
-           reversed= reversed*10+digit;
-           temp /= 10;
-       }
-       return (reversed == x);
+        String str = String.valueOf(x);
+        String reversedStr = new StringBuilder(str).reverse().toString();
+        return str.equals(reversedStr);
     }
 }
