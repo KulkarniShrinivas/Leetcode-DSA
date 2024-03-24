74406bfcf2fc787025b652d85089d728bcd6cd46
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-03-24 12:41:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (86.8%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,40 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rowIdx = searchPotentialRow(matrix, target);
-        if(rowIdx != -1) return binarySearchOverRow(rowIdx, matrix, target);
-        else return false;
-    }
-    private int searchPotentialRow(int[][] matrix, int target){
-        int low=0;
-        int high=matrix.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
 
-            if(matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length-1]){
-                return mid;
-            }
-            else if (matrix[mid][0] < target) low=mid+1;
-            else if(matrix[mid][0] > target) high=mid-1;
-        }
-        return -1;
-    }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
 
-    private boolean binarySearchOverRow(int rowIdx, int[][] matrix, int target){
-        int low=0;
-        int high=matrix[rowIdx].length-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-
-            if(matrix[rowIdx][mid] == target) return true;
-            else if(matrix[rowIdx][mid] > target) high=mid-1;
-            else low=mid+1;
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
     }
 }
-
-//will be finding potential row to apply binary search 
-//later will apply binarry seary or searchPotential row to check exactly the potential row 
-//will apply binary serch in 1st column and will get to know the potential row 
-//as it is sorted so 23 is less than 10 so it will be present in 1st column 
-//so will be applying binary serch in row 1 and will get the target 
