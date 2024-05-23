0424fd71511fe7ed78285968770ddd8fdabc3717
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-05-23 21:39:10 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,15 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-      int p1=m-1, p2=n-1, i=m+n-1;
-      while(p2 >= 0){
-        if(p1 >= 0 && nums1[p1] > nums2[p2]){
-            nums1[i--]=nums1[p1--];
-        } else{
-            nums1[i--] = nums2[p2--];
+     int m1=m-1, n1=n-1, mn=m+n-1;
+     while(m1>=0 && n1>=0){
+        if(nums1[m1] > nums2[n1]){
+            nums1[mn] = nums1[m1];
+            m1--;
         }
-      }
+        else{
+            nums1[mn] = nums2[n1];
+            n1--;
+        }
+        mn--;
+     }
+     while(n1>=0){
+        nums1[mn]=nums2[n1];
+        n1--;
+        mn--;
+     }
     }
 }
+
 
 //two pointer approach
 //1 2 3 0 0 0       2 5 6
