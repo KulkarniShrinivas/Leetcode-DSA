654ae1fbdd9d4c62a9a2b275ae1d2058b6f8cda5
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-08-06 17:05:36 +0530

    Time: 0 ms (100%), Space: 42 MB (72.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -13,3 +13,12 @@ class Solution {
         }
     }
 }
+//two pointer approach
+//1 2 3 0 0 0       2 5 6
+
+//p1=m-1 p2=n-1
+//6>3 so it will go at the end of the array 1 2 3 0 0 6
+//nums2[p2]>num1[p1]
+//nums1[i]=num2[p2]
+
+//indexes are decremented
