0802a3a7f149d07291fd5c5be95d9f59d3554da7
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-11-08 23:58:25 +0530

    Time: 2 ms (61.76%), Space: 41.4 MB (57.3%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,21 +1,14 @@
 class Solution {
-    public boolean isSubsequence(String str1, String str2) {
-        //intitalize two pointer
-        int i=0;
-        int j=0;
+    public boolean isSubsequence(String s, String t) {
+         int i = 0;
+        int j = 0;
 
-        while(i<str1.length() && j<str2.length()){
-            //comapre characters, increament both pointers
-            if(str1.charAt(i) == str2.charAt(j)){
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
                 i++;
-                j++;
-
-            }else{
-                j++; //only increment second pr
             }
+            j++;
         }
-        //if length of a subsequence 'i' will hvae travelled full
-        //length of string str so just check and return 
-        return(i == str1.length());
+        return i == s.length();
     }
 }
