691e93a61c76594cb1180415f6be02324014c17e
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-11-09 10:13:06 +0530

    Time: 1 ms (81.33%), Space: 41.5 MB (20.91%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> reverseMap = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                map.put(c, word);
+            }
+
+            if (reverseMap.containsKey(word)) {
+                if (reverseMap.get(word) != c) {
+                    return false;
+                }
+            } else {
+                reverseMap.put(word, c);
+            }
+        }
+
+        return true; 
+    }
+}
