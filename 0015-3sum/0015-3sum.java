b50ab51a7f7b76d8bf70a7e23b81b98cc2f7b863
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-12-12 10:36:22 +0530

    Time: 32 ms (43.25%), Space: 51.1 MB (87.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,42 @@
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
 
-        //sort
         Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-2; i++){
-            //skip duplicates for i
-            if(i> 0 && nums[i] == nums[i-1]){
+        for (int i = 0; i < nums.length - 2; i++) {
+            
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int j=i+1;
-            int k=nums.length-1;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-
-                if(sum == 0){
-                    //found triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    //skip duplicates from j
-                    while(j<k && nums[j] == nums[j + 1]){
-                        j++;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
                     }
-                    //skip duplicates elements from k
-                    while(j < k && nums[k] == nums[k-1]){
-                        k--;
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
                     }
-
-                    //move pointers
-                    j++;
-                    k--;
-                } else if(sum<0){
-                    //sum is less than zero, increment j to increase the sum
-                    j++;
-                } else{
-                    //sum is greater than 0 decreament k
-                    k--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
-            
         }
-        return ans;
-        
+        return result;
     }
 }
